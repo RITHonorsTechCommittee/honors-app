97b53fb4cd5cfd2c7242a98971d50cb7fd26fa35
Randy Carlson <randycarlson486@gmail.com>
2019-02-27 21:05:17 -0500

    made models

--- honors_app/models.py
+++ honors_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(max_length=256)
+    leaders = models.ManyToManyField(User, related_name='leader_of')
+    members = models.ManyToManyField(User, related_name='member_of')
+
+class Event(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    attendees = models.ManyToManyField(User)
+    organization = models.ForeignKey(Organization, null=True, on_delete=models.SET_NULL)
+    private = models.BooleanField()
