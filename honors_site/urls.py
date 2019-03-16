71e342d0a19ea131242aa61f9c723b003b47a5d6
Randy Carlson <randycarlson486@gmail.com>
2019-03-15 18:53:53 -0700

    added login form

--- honors_site/urls.py
+++ honors_site/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('honors_app.urls')),
     path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
 ]
