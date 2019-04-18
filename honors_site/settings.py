0b2528349f893b421845e76ff553133900bc19d4
randy <randycarlson486@gmail.com>
2019-04-17 20:40:25 -0400

    changed debug and secret_key to use env vars

--- honors_site/settings.py
+++ honors_site/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@&td0e2$6szf67%$1_ghtz7*0$p+@ujaif)b*1#p_7oq-6rp%l'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False').upper() == 'TRUE'
 
 ALLOWED_HOSTS = []
 
