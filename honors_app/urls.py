37cc786f8d33eb1e7ddfa9ee0c9f9eaa02ae2a59
randy <randycarlson486@gmail.com>
2019-04-19 01:27:12 -0400

    polished events

--- honors_app/urls.py
+++ honors_app/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
-    path('event/<int:pk>/', views.event_details, name='event-detail'),
-    path('accounts/login/', views.login),
+    path('', views.homepage, name='home'),
+    path('events/<pk>/edit/', views.event_edit, name='event-edit'),
+    path('accounts/login/', views.login, name='login'),
     path('events/', views.events_overview, name='events'),
-    path('events/<int:pk>/', views.event_view, name='event-view'),
+    path('events/<pk>/', views.event_view, name='event-view'),
 ]
--- honors_app/views.py
+++ honors_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
 from .models import Event, Organization
@@ -11,21 +12,26 @@ def homepage(request):
     return render(request, 'homepage.html')
 
 
-def event_details(request, pk):
+def event_edit(request, pk):
     if request.method == 'POST':
-        form = EventForm(request.POST, instance=get_object_or_404(Event, pk=pk))
+        if pk != 'new':
+            form = EventForm(request.POST, instance=get_object_or_404(Event, pk=pk))
+        else:
+            form = EventForm(request.POST)
         if form.is_valid():
             #TODO: add check for sequence, user permsissions
-            form.save()
-            return HttpResponseRedirect(request.path)
+            event = form.save()
+            return HttpResponseRedirect(reverse('event-view', args=[event.pk]))
 
     else:
         if pk == 'new':
+            instance = None
             form = EventForm()
         else:
-            form = EventForm(instance=get_object_or_404(Event, pk=pk))
+            instance = get_object_or_404(Event, pk=pk)
+            form = EventForm(instance=instance)
 
-    return render(request, 'event_detail.html', {'form': form})
+    return render(request, 'event_edit.html', {'form': form, 'instance': instance})
 
 def login(request):
     return render(request, 'login_oauth.html')
